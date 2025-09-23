50c25a5e01b7cf3c139742a4bcb430af8cb2a44c
Fernandes Samuel <glazvonkrovi@gmail.com>
2025-09-23 09:56:57 +0200

    TestRecognizesAccents

--- spliter/spliter_test.go
+++ spliter/spliter_test.go
@@ -12,3 +12,12 @@ func TestExtractArgs(t *testing.T) {
 		t.Error("ExtractArgs should return '", args, "' but returned '", ExtractArgs(uri), "'")
 	}
 }
+
+func TestRecognizesAccents(t *testing.T) {
+	const uri = "microzilla://T%C3%A9l%C3%A9chargements/bridge.dxf"
+	const args = "Téléchargements/bridge.dxf"
+
+	if args != ExtractArgs(uri) {
+		t.Error("ExtractArgs should return '", args, "' but returned '", ExtractArgs(uri), "'")
+	}
+}
